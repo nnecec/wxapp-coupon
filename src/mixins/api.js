651937691fed4e8fc3a887974ea5289a50d87bfa
nnecec <shine_c@outlook.com>
2017-04-26 19:19:10 +0800

    增加转发 二维码添加参数

--- src/mixins/api.js
+++ src/mixins/api.js
@@ -1,19 +1,19 @@
 import wepy from 'wepy'
 
-const wxRequest = async (params = {}, url) => {
+const wxRequest = async (params = {}, url, agentId = '') => {
   wx.showToast({
     title: '加载中',
     icon: 'loading'
   })
-  console.log(params, url)
   let res = await wepy.request({
     url: url,
     method: params.method || 'GET',
     data: params.data || {},
     header: {
       'Content-Type': 'application/json',
-      'X-Version': '1.0',
-      'X-AppName':'fansCoupon'
+      'X-Version': '1.1',
+      'X-AppName':'fansCoupon',
+      'x-agent-app-id': agentId
     }
   })
   wx.hideToast()
@@ -21,7 +21,7 @@ const wxRequest = async (params = {}, url) => {
   return res
 }
 
-const fetchDetail = (params, url) => wxRequest(params, url)
+const fetchDetail = (params, url, agentId) => wxRequest(params, 'https://adbaitai.com' + url, agentId)
 
 module.exports = {
   fetchDetail,
